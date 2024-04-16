6637cde6d3f48bb85010e5ab9a6f9404e8d1f3be
Matthew Egan <mnegan93@gmail.com>
2024-04-16 18:37:42 +1200

    Create data with field structure

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const fields = [
+    {
+        id: '1',
+        map_to_field: false,
+        field: '', // Which field to map to
+        type: 'Single Line Text',
+        mandatory: false,
+        active: true,
+        order: 1,
+
+        question: {
+            question_text: 'question_text',
+            name: 'name'
+        },
+
+        annotation: {
+            show_help: true,
+            always_expanded: true,
+            help_tag
+        },
+
+        type_specifications: {
+            variable_width: '50%'
+        },
+
+        comments_for_developers: ''
+    }
+]
